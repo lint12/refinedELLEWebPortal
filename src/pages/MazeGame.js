383c55bd0ee985fbfc9e932e3b68983d29cd1633
notis404 <nathaneotis@gmail.com>
2022-04-11 19:49:41 -0400

    New MazeGame Build

--- src/pages/MazeGame.js
+++ src/pages/MazeGame.js
@@ -14,10 +14,10 @@ import banner from '../Images/ELLEDownloadsBanner.mp4';
 import Unity, { UnityContext } from "react-unity-webgl";
 
 const unityContext = new UnityContext({
-  loaderUrl: 'Build/DevBuild411.loader.js',
-  dataUrl: 'Build/DevBuild411.data',
-  frameworkUrl: 'Build/DevBuild411.framework.js',
-  codeUrl: 'Build/DevBuild411.wasm',
+  loaderUrl: 'Build/DevBuild411_2.loader.js',
+  dataUrl: 'Build/DevBuild411_2.data',
+  frameworkUrl: 'Build/DevBuild411_2.framework.js',
+  codeUrl: 'Build/DevBuild411_2.wasm',
 });
 
 export default class MazeGame extends Component {
